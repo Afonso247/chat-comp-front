03bfecd80907068737ed59b94606b979530b1094
afonso.araujo <afonso.araujo@supertroco.com.br>
2025-07-09 22:36:58 -0300

    console.log

--- src/App.vue
+++ src/App.vue
@@ -171,6 +171,7 @@ export default {
       }
 
       try {
+        console.log(import.meta.env.VITE_API_URL)
         const response = await axios.post(`${import.meta.env.VITE_API_URL}/api/responses`, {
           prompt: this.prompt,
         })
